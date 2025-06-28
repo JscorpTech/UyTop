afee7814858fa38e60ec37b1cc9906b1fc74d455
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-06-28 11:15:25 +0500

    New g4f version: 0.5.5.7 (current: 0.3.9.7) | pip install -U g4f

--- core/apps/api/urls.py
+++ core/apps/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from core.apps.api.views import ListingView, ListingimageView, CategoryView
+from core.apps.api.views import ListingView, ListingimageView, CategoryView, AmenityView
 
 
 router = DefaultRouter()
@@ -8,6 +8,7 @@ router.register(r"listing", ListingView, basename="listing")
 router.register(r"listing-image", ListingimageView, basename="Listingimage")
 
 router.register("category", CategoryView, basename="category")
+router.register("amenity", AmenityView, basename="amenity")
 
 
 
