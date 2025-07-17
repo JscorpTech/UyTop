1ff8ae3ddceb75abb91cfb1567f27e227158a528
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-07-17 14:15:00 +0500

    New g4f version: 0.5.7.3 (current: 0.3.9.7) | pip install -U g4f

--- core/apps/api/admin/listingImage.py
+++ core/apps/api/admin/listingImage.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin
-
 from core.apps.api.models import ListingimageModel
-
+from django.utils.safestring import mark_safe
 
 @admin.register(ListingimageModel)
 class ListingimageAdmin(ModelAdmin):
     list_display = (
         "id",
-        "__str__",
+        "listing",
+        "image_tag",
     )
+
+    def image_tag(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" style="height: 70px; border-radius: 8px; object-fit: cover;" />')
+        return "-"
+    
+    image_tag.short_description = "Image"
