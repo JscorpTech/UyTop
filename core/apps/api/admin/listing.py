541f93049104e61c378b93d6b15024861a164ac9
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-07-17 14:18:53 +0500

    New g4f version: 0.5.7.3 (current: 0.3.9.7) | pip install -U g4f

--- core/apps/api/admin/listing.py
+++ core/apps/api/admin/listing.py
@@ -13,6 +13,8 @@ class ListingImageInlines(UnfoldTabulerAdmin):
 
 
 
+from django.utils.html import format_html
+
 @admin.register(ListingModel)
 class ListingAdmin(ModelAdmin, TabbedTranslationAdmin):
     list_display = (
@@ -22,11 +24,20 @@ class ListingAdmin(ModelAdmin, TabbedTranslationAdmin):
         "floor",
         "is_active",
         "repair_type",
-        "building"
+        "building",
+        "show_images"  # Rasmlarni chiqaradigan method
     )
     inlines = [ListingImageInlines]
-    list_display_links = ("name"),
+    list_display_links = ("name",)
 
+    def show_images(self, obj):
+        images = obj.listingimagemodel_set.all()[:3]  # Faqat 3 ta rasm ko‘rsatish uchun
+        imgs = ""
+        for img in images:
+            imgs += f'<img src="{img.image.url}" width="50" style="margin-right:5px;" />'
+        return format_html(imgs)
+    
+    show_images.short_description = "Rasmlar"
 
 
     
