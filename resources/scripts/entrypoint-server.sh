ebb7fd697e03ba68f7a0e80af9249f11d7efa3a1
A'zamov Samandar <admin@jscorp.uz>
2025-09-08 20:11:14 +0500

    prod

--- resources/scripts/entrypoint-server.sh
+++ resources/scripts/entrypoint-server.sh
@@ -5,10 +5,15 @@ while ! nc -z db 5432; do
   echo "Waiting postgress...."
 done
 
+while ! nc -z redis 6379; do
+  sleep 2
+  echo "Waiting redis...."
+done
+
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
 
-gunicorn config.wsgi:application -b 0.0.0.0:8000 --workers $(($(nproc) * 2 + 1))
+gunicorn config.wsgi:application -b 0.0.0.0:8000 --workers 6
 
 
 
